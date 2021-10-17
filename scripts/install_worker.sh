3ca4cba4eaf9e4a7620995a7f66db42dbee03391
Gizem Gur <gizem@sufle.io>
2021-10-18 01:59:54 +0300

    cleanup change for inventory association step

--- scripts/install_worker.sh
+++ scripts/install_worker.sh
@@ -335,23 +335,14 @@ if [[ "$CLEANUP_IMAGE" == "true" ]]; then
 
     # Clean up files to reduce confusion during debug
     sudo rm -rf \
-        /etc/hostname \
-        /etc/machine-id \
-        /etc/resolv.conf \
-        /etc/ssh/ssh_host* \
         /home/ec2-user/.ssh/authorized_keys \
         /root/.ssh/authorized_keys \
-        /var/lib/cloud/data \
-        /var/lib/cloud/instance \
-        /var/lib/cloud/instances \
-        /var/lib/cloud/sem \
-        /var/lib/dhclient/* \
-        /var/lib/dhcp/dhclient.* \
         /var/lib/yum/history \
         /var/log/cloud-init-output.log \
         /var/log/cloud-init.log \
-        /var/log/secure \
         /var/log/wtmp
+
+    sudo echo '' > /var/log/secure
 fi
 
 sudo touch /etc/machine-id
